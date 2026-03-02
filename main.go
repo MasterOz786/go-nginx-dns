1895d19808b2bef29936e31c72fb646b697d78f1
MasterOz786 <hassanejaz400@gmail.com>
2026-03-02 07:12:21 +0500

    chore: basic dns lookups endpoint

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
-
 package main
 
-import "fmt"
+import (
+	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/MasterOz786/go-nginx-dns/internal/handlers"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "OK"})
+	})
+
+	r.GET("/dns", handlers.GetDNSInfo)
+
+	log.Println("Server running!")
+	r.Run(":8080")
 }
